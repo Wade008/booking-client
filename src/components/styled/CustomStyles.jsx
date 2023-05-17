5d32f98d9cf248add9c8b750b1df26de8b490a60
Wade Doolan <wadedoolan@hotmail.com>
2023-05-17 19:19:43 +1000

    Tidy up files

--- src/components/styled/CustomStyles.jsx
+++ src/components/styled/CustomStyles.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import px2vw from "../utils/px2vw";
+
 
 
 export const Image = styled.img`
@@ -7,17 +7,3 @@ export const Image = styled.img`
 
 `
 
-
-export const Container = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin:0 ${px2vw(30)} ${px2vw(30) };
-    padding-top: 90px;
-    max-width: 100%;
-   
-   
-    @media (min-width: 1024px) {
-    flex-wrap: nowrap;
-  }
-`
